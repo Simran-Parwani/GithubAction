b8bc3e742b71830d9b2f26d3ba6c19337c715253
simran-parwani <sparwani@cloudsmith.io>
2025-03-16 22:49:10 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypackage",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    author_email="your@email.com",
+    description="A sample Python package",
+    long_description="This is a sample Python package for demo purposes.",
+    url="https://github.com/yourname/mypackage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
